f9d98a510ef746c575c0448d52d73b051987912c
LeeYoungMin <miniyoung37@gmail.com>
2020-04-03 02:19:52 +0900

    add contentpage

--- firma/src/App.js
+++ firma/src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import "styles/main.scss";
 
-import Footer from "./component/Footer";
-import LandingPage from "./pages/landingPage";
-import TestimonialPage from "./pages/testimonialPage";
-import CommercePage from "./pages/commercePage";
+import Footer from "component/Footer";
+import LandingPage from "pages/landingPage";
+import TestimonialPage from "pages/testimonialPage";
+import ContentPage from "pages/contentPage";
+import CommercePage from "pages/commercePage";
 
 function App() {
   return (
     <>
       <LandingPage />
       <TestimonialPage />
+      <ContentPage />
       <CommercePage />
       <Footer />
     </>
