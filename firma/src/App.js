284333c16fbeb1dc4cb3e4556e97621a4f57f5a0
LeeYoungMin <miniyoung37@gmail.com>
2020-04-03 03:02:09 +0900

    add blogpage

--- firma/src/App.js
+++ firma/src/App.js
@@ -5,6 +5,7 @@ import Footer from "component/Footer";
 import LandingPage from "pages/landingPage";
 import TestimonialPage from "pages/testimonialPage";
 import ContentPage from "pages/contentPage";
+import BlogPage from "pages/blogPage";
 import CommercePage from "pages/commercePage";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <LandingPage />
       <TestimonialPage />
       <ContentPage />
+      <BlogPage />
       <CommercePage />
       <Footer />
     </>
